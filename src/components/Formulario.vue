ebb2d9950abf4fd07b80a65ba450af143e319f95
kaynansn11@gmail.com <kaynansn11@gmail.com>
2023-06-12 15:45:11 -0300

    commit again

---
+++ src/components/Formulario.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps(['cadastraTarefa', 'tarefaTemp', 'trocarFiltro', 'editarTarefaTemp']);
+</script>
+
+<template>
+  <form @submit.prevent="props.cadastraTarefa">
+    <div class="row">
+      <div class="col">
+        <input v-model="props.tarefaTemp" required type="text" placeholder="Digite aqui a descrição da tarefa"
+          class="form-control">
+      </div>
+      <div class="col-md-2">
+        <button type="submit" class="btn btn-primary">Cadastrar</button>
+      </div>
+      <div class="col-md-2">
+        <select @change="props.trocarFiltro" class="form-control">
+          <option value="todas">Todas tarefas</option>
+          <option value="pendentes">Pendentes</option>
+          <option value="finalizadas">Finalizadas</option>
+        </select>
+      </div>
+    </div>
+  </form>
+</template>
